986115fbcdb22d00ceec934f827ffb67e5c7e5b2
Liuga92 <luc.92@hotmail.it>
2015-01-09 12:58:41 +0100

    repo quasi finito

--- ProjCyclon/analysis/data.py
+++ ProjCyclon/analysis/data.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
 
 
-	folder = "C:/Users/Liuga/Google Drive/magistrale/Algo dist/csv/normal/p30s10/m20000"
+	folder = "F:"
 
 	fl = listdir(folder)
 	for x in reversed(range(0,len(fl))):
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 	fl.sort(key=sorter)
 	
 	
-	f = open("self_cleaning_capacity10-30.tsv","w")
+	f = open("degree_distribution.tsv","w")
 	normal = csv.writer(f,delimiter="\t",lineterminator="\n")
 
 
@@ -48,39 +48,48 @@ if __name__ == '__main__':
 	
 	#------------------------------
 
+	#start = 30
 
+	#for i in fl[start:] :
 
+	#	opening = open_csv(folder+"/"+i)
 
-	for i in fl[0:] :
-
-		opening = open_csv(folder+"/"+i)
+	#	#print(len(marked_peers(opening)))
 		
-		n = int(i.split(".")[0])
+	#	n = int(i.split(".")[0])
 		
-		#g = create_graph(opening)
+	#	g = create_graph(opening)
 
-		#a = attack_resistance(g,opening)
+	#	a = attack_resistance(g,opening)
 
-		s =self_cleaning_capacity(opening)
-		normal.writerow([n-0,s])
-		print(n-0,s)
-
-	
-	#gr =nx.gnm_random_graph(20000,len(g.edges()))
+	#	#s =self_cleaning_capacity(opening)
+	#	normal.writerow([n-start,a])
+	#	print(n-start,a)
 
 
-	#r = robustness_test(gr,minimum=60.0,distance=0.5)
+
+	#opening = open_csv(folder+"/30.csv")
+		
+		
+	#g = create_graph(opening)
+
+
+	## r = robustness_test(gr,minimum=60.0,distance=0.5)
 	#d = degree_distribution(gr)
+	#print(d)
 	
 	#for i in d :
+	#	normal.writerow(list(i))
 
 	
 	#------------------------------------------
-	#opening = open_csv(folder+"/40.csv")
-	#g = create_graph(opening,directed=True)
-	#d = degree_distribution(g)
-	#for i in d :
-	#	normal.writerow(list(i))
+	opening = open_csv(folder+"/41.csv")
+	g = create_graph(opening,directed=True)
+	#gr =nx.gnm_random_graph(5000,len(g.edges()),directed=True)
+	d = degree_distribution(g)
+	for i in d :
+		normal.writerow(list(i))
+		print(i)
 
 
 	#--------------------------------------------
