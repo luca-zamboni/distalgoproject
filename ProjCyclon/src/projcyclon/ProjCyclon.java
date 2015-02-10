46744d460d8fc3a38cadd22e7fffceb7c384a677
Liuga92 <luc.92@hotmail.it>
2015-02-10 17:38:26 +0100

    Revert "Revert "tolti errori di grammatica dai jpanel""
    This reverts commit 7644a6fa13fc1f23c5c81d8f3f7c88763cd31229.

--- ProjCyclon/src/projcyclon/ProjCyclon.java
+++ ProjCyclon/src/projcyclon/ProjCyclon.java
@@ -36,8 +36,8 @@ public class ProjCyclon {
         // DECOMMENTARE QUANDO CONSEGNAMO
         try {
             String sNPeer = JOptionPane.showInputDialog("Number of Peers?");
-            String snei = JOptionPane.showInputDialog("Neighboars");
-            String sscamb = JOptionPane.showInputDialog("Changed?");
+            String snei = JOptionPane.showInputDialog("Number of neighbors?");
+            String sscamb = JOptionPane.showInputDialog("Number of exchanged peers?");
             NPeer = Integer.parseInt(sNPeer);
             nei = Integer.parseInt(snei);
             scamb = Integer.parseInt(sscamb);
